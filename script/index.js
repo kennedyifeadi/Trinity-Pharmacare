270a2c94e7975bc5946337cf1fd9f5179fa88da9
kennedyifeadi <kennedyifeadi@gmail.com>
2024-05-05 15:17:24 +0100

    update on homePage

--- script/index.js
+++ script/index.js
@@ -7,3 +7,27 @@ menuBar.addEventListener('click', ()=>{
 closeMenuBar.addEventListener('click', ()=>{
     navBarNavDetailsDirectoryMobileView.classList.toggle('showdetails')
 })
+
+const nav_BarNavDetailsSearchAndContactSearchbar = document.getElementById('nav_Bar-navDetails-searchAndContact_searchbar');
+const searchBarClose = document.getElementById('searchBar_close');
+const nav_BarNavDetailsSearchAndContactSearchBarCover = document.getElementById('nav_Bar-navDetails-searchAndContact_SearchBarCover');
+nav_BarNavDetailsSearchAndContactSearchbar.addEventListener('click', ()=>{
+    nav_BarNavDetailsSearchAndContactSearchBarCover.classList.toggle('showdetails')
+});
+searchBarClose.addEventListener('click', ()=>{
+    nav_BarNavDetailsSearchAndContactSearchBarCover.classList.toggle('showdetails')
+})
+const searchBar = document.getElementById('searchBar')
+searchBar.addEventListener('click', ()=>{
+    nav_BarNavDetailsSearchAndContactSearchBarCover.classList.toggle('showdetails')
+});
+
+
+
+const dropDown = document.getElementById('nav_Bar-navDetails-directory_mobileViewLink_dropdown');
+const dropDownDetails = document.getElementById('nav_Bar-navDetails-directory_mobileViewLink_dropdownDetails');
+dropDown.addEventListener('click', ()=>{
+    dropDownDetails.classList.toggle('dropdownShown')
+console.log(dropDownDetails);
+
+})
